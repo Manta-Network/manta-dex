92152d1b3c02a441fcc8e8713825e4fcee9f0135
Vern Zhang <vern@manta.network>
2024-02-27 22:44:03 +0800

    chore: update MANDX to MANDEX

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -64,7 +64,7 @@ export default function Index() {
                   <Image priority width={100} src={IconB} alt="Trade Now" />
                 </div>
                 Supply a token pair to receive liquidity provider (LP) tokens.
-                Stake LP tokens to mine MANDX! Be sure to catch up with the
+                Stake LP tokens to mine MANDEX! Be sure to catch up with the
                 First Mine Period!
               </div>
               <a href="/pool">
@@ -80,7 +80,7 @@ export default function Index() {
                 <div className="pb-2">
                   <Image priority width={100} src={IconC} alt="Trade Now" />
                 </div>
-                You can stake MANDX tokens to earn fees and MANDX token rewards.
+                You can stake MANDEX tokens to earn fees and MANDEX token rewards.
               </div>
               <a href="/pool">
                 <Button fullWidth disabled size="sm">
